36c6c3abb4b6c9db1cdb0de6467742a511c6c0b6
Marc <m.h@haw.de>
2015-11-05 10:52:22 +0100

    Commited

---
+++ ImageEditorFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+public class ImageEditorFrame extends JFrame{
+	public static void main(String[] args){
+	}
+	
+	public ImageEditorFrame(){
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setVisible(true);
+	}
+ 
+}
